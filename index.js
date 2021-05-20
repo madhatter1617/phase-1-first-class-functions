1a69f0a769ce4dc009ba0f4987a8f7369342b086
Rosa <rosarenee@att.net>
2021-05-19 19:52:44 -0500

    stuck on anonmyous

---
+++ index.js
@@ -0,0 +1,31 @@
+function receivesAFunction(callback) {
+callback();
+
+}
+function newLuRat(){
+   let Kachaaa = "love it!"
+}
+function newKat(){
+   
+   return newLuRat
+}
+function returnsANamedFunction(){
+ return newKat()
+ 
+}
+// function teaParty(){
+//    let dead = '';
+//    return dead 
+// }
+function returnsAnAnonymousFunction(){
+   
+const sayBye = function() {
+   return "bye Bi***!"
+   
+}
+
+
+
+  }
+   
+
